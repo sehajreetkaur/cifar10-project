b43edc114d249ea0f1121127cecea5c77720e6be
sariosa <sara.ghahremany76@gmail.com>
2026-04-03 13:54:18 +0200

    Loading data from CIFAR-10 dataset...

---
+++ src/data_loader.py
@@ -0,0 +1,18 @@
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+
+def load_cifar10():
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+    x_train = x_train.astype("float32") / 255.0
+    x_test = x_test.astype("float32") / 255.0
+
+    y_train = to_categorical(y_train, 10)
+    y_test = to_categorical(y_test, 10)
+
+    class_names = [
+        "airplane", "automobile", "bird", "cat", "deer",
+        "dog", "frog", "horse", "ship", "truck"
+    ]
+
+    return x_train, y_train, x_test, y_test, class_names
